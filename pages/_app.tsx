02a751d4ca34eaea6809d484e9fff43c93780fd0
eljerrywhite <eljerrywhite@gmail.com>
2025-07-02 01:19:50 +0000

    Elimina importación de @next/third-parties y actualiza dependencias

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,7 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <GoogleAnalytics gaId="G-XVGYGJKCMN" /> {/* Ejemplo: tu ID real */}
-    </>
-  );
+  return <Component {...pageProps} />;
 }
